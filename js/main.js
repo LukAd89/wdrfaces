853eb0b0e89a332ebcf366a9b66fb1ef05a85daf
unknown <mrskigh@gmail.com>
2019-04-03 03:50:28 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+function getProfileData(id){
+  return new Promise((resolve, reject) => {
+    $.getJSON('profiles/dummy.json', data => {
+      resolve(data[id]);
+    });
+  });
+}
+
+async function initiateProfile(id) {
+  let profile_data = await getProfileData(id);
+  console.log("data", JSON.stringify(profile_data.name))
+  await $('.card-header').html(profile_data.name);
+  console.log("jo")
+}
+
 (function ($) {
+  //= require('../data/profiles.json');
   $('#nav-link-tables').click(function () {
     $('#page-content').load("sections/trainees.html");
   });
@@ -10,4 +26,9 @@
     $('#page-content').load("sections/blank.html");
   });
 
+  $(document).on("click", ".profile-card", async function(){
+    console.log($(this).data("profileid"));
+    await $('#page-content').load("sections/profile.html");
+    initiateProfile($(this).data("profileid"));
+  });
 })(jQuery);

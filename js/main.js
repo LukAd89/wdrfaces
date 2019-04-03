6c99d317c110b0f9fe5206ad8811d1403143c673
MrSkigh <mrskigh@gmail.com>
2019-04-03 20:38:59 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-function getProfileData(id){
+function getProfileData(id) {
   return new Promise((resolve, reject) => {
-    $.getJSON('profiles/dummy.json', data => {
+    $.getJSON('data/profiles.json', data => {
       resolve(data[id]);
     });
   });
@@ -8,15 +8,28 @@ function getProfileData(id){
 
 async function initiateProfile(id) {
   let profile_data = await getProfileData(id);
-  console.log("data", JSON.stringify(profile_data.name))
-  await $('.card-header').html(profile_data.name);
-  console.log("jo")
+  await $('.card-header').text(profile_data.name);
+  $('.trn-name').text(profile_data.name);
+  $('.trn-studies').text(profile_data.studies);
+  $('.trn-stage').text(profile_data.stage);
+}
+
+async function initiateTrainees() {
+  for (let i = 0; i < 10; i++) {
+    let clone = await $($("#profileCardTemplate").html());
+    await $('.trn-name', clone).text("Trainee Nr. " + (i+1));
+    await $('.profile-card', clone).attr("data-profileid", "trn" + i)
+    await $('.trow-1').append(clone);
+  }
 }
 
 (function ($) {
   //= require('../data/profiles.json');
-  $('#nav-link-tables').click(function () {
-    $('#page-content').load("sections/trainees.html");
+  // $('#nav-link-tables').click(async function () {
+  $(document).on("click", '#nav-link-tables', async function () {
+    await $('#page-content').load("sections/trainees.html", async () => {
+      await initiateTrainees();
+    });
   });
   $('#nav-link-charts').click(function () {
     $('#page-content').load("sections/blank.html");
@@ -26,9 +39,9 @@ async function initiateProfile(id) {
     $('#page-content').load("sections/blank.html");
   });
 
-  $(document).on("click", ".profile-card", async function(){
-    console.log($(this).data("profileid"));
-    await $('#page-content').load("sections/profile.html");
-    initiateProfile($(this).data("profileid"));
+  $(document).on("click", ".profile-card", async function () {
+    await $('#page-content').load("sections/profile.html", async () => {
+      initiateProfile($(this).data("profileid"));
+    });
   });
 })(jQuery);

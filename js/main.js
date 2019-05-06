0bcd4bb74e7ef81b6473c865671528f39f17d07a
MrSkigh <mrskigh@gmail.com>
2019-05-06 18:18:54 +0200

    1

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ async function initiateTrainees() {
 
   $(document).on("click", '#nav-link-quiz', async function () {
     await $('#page-content').load("sections/quiz.html", async () => {
-      $.getScript("/js/quiz.js"), async () =>{
+      await $.getScript("/js/quiz.js"), async () =>{
         await start();
       };
     });

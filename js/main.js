1a593eba11d4add3a24a3b72906f783be862645c
MrSkigh <mrskigh@gmail.com>
2019-05-09 08:19:06 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ async function initiateProfile(id) {
 async function initiateTrainees() {
   for (let i = 0; i < 10; i++) {
     let clone = await $($("#profileCardTemplate").html());
-    await $('.trn-name', clone).text("Trainee Nr. " + (i+1));
+    await $('.trn-name', clone).text("Trainee Nr. " + (i + 1));
     await $('.profile-card', clone).attr("data-profileid", "trn" + i)
     await $('.trow-1').append(clone);
   }
@@ -26,22 +26,33 @@ async function initiateTrainees() {
 (function ($) {
   //= require('../data/profiles.json');
   // $('#nav-link-tables').click(async function () {
+
+  $(document).on("click", '#nav-link-start', async function () {
+    await $('#page-content').load("sections/start.html", async () => {});
+    $("#accordionSidebar .nav-item.active").removeClass("active");
+    $("#nav-link-start").parent(".nav-item").addClass("active");
+  });
+
   $(document).on("click", '#nav-link-trainees', async function () {
     await $('#page-content').load("sections/trainees.html", async () => {
       await initiateTrainees();
     });
+    $("#accordionSidebar .nav-item.active").removeClass("active");
+    $("#nav-link-trainees").parent(".nav-item").addClass("active");
   });
 
   $(document).on("click", '#nav-link-quiz', async function () {
     await $('#page-content').load("sections/quiz.html", async () => {
       await $.getScript("js/quiz.js")
-      .done(function( script, textStatus ) {
-        console.log( textStatus );
-      })
-      .fail(function( jqxhr, settings, exception ) {
-        console.log( "Triggered ajaxError handler.", exception);
-    })
+        .done(function (script, textStatus) {
+          console.log(textStatus);
+        })
+        .fail(function (jqxhr, settings, exception) {
+          console.log("Triggered ajaxError handler.", exception);
+        })
     });
+    $("#accordionSidebar .nav-item.active").removeClass("active");
+    $("#nav-link-quiz").parent(".nav-item").addClass("active");
   });
 
   $('.nav-link:not([id])').click(function () {
@@ -53,4 +64,16 @@ async function initiateTrainees() {
       initiateProfile($(this).data("profileid"));
     });
   });
+
+  $(document).on("click", '#trn-back-button', async function () {
+    await $('#page-content').load("sections/trainees.html", async () => {
+      await initiateTrainees();
+    });
+    $("#accordionSidebar .nav-item.active").removeClass("active");
+    $("#nav-link-trainees").parent(".nav-item").addClass("active");
+  });
+
+  $( document ).ready(function() {
+    $('#page-content').load("sections/start.html", async () => {});
+});
 })(jQuery);
